ba73d2688c44bbb99ad3cc324bb6de1b52f27d01
Ikenna <Ikenna.ogbo@hotmail.com>
2019-01-21 19:27:18 +0000

    Updates

---
+++ precache-manifest.2c94dbc8185f8f363c6c6cc47af1ac34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf388b0f515924e67137",
+    "url": "/Todolist/static/js/runtime~main.bf388b0f.js"
+  },
+  {
+    "revision": "b864c2b8676ac8fe7494",
+    "url": "/Todolist/static/js/main.b864c2b8.chunk.js"
+  },
+  {
+    "revision": "e0bf5a2fbfd6476d7051",
+    "url": "/Todolist/static/js/1.e0bf5a2f.chunk.js"
+  },
+  {
+    "revision": "b864c2b8676ac8fe7494",
+    "url": "/Todolist/static/css/main.bdfff844.chunk.css"
+  },
+  {
+    "revision": "e31c6c69893f921b1ad8a81ec48647d0",
+    "url": "/Todolist/index.html"
+  }
+];
